4fd754bade836de02b02f32c7fd8bf9714080125
Fletcher Watson <fbwthe6@gmail.com>
2016-09-14 12:06:26 -0500

    whoops. worked on master briefly. created createParty function in PartyFactory.js

--- scripts/Factories/PartyFactory.js
+++ scripts/Factories/PartyFactory.js
@@ -2,9 +2,33 @@
 
 app.factory("PartyFactory", function($q, $http, FirebaseURL, $location){
 
-  let createParty = () => {
+  let createParty = (newParty) => {
+    return $q((resolve, reject) => {
+      $http.post(`${FirebaseURL}/parties.json`,
+        JSON.stringify(newParty))
+      console.log(newParty, "new Party")
+        .success((partyObjFromFirebase) => {
+          resolve(partyObjFromFirebase)
+          })
+        .error((error) => {
+          reject(error)
+        })
+      })
+    }
 
-  }
+    let createProfile = (newProfile) => {
+    return $q((resolve, reject) => {
+      $http.post(`${FirebaseURL}/profiles.json`,
+        JSON.stringify(newProfile))
+      console.log(newProfile, "new Profile")
+        .success((profileObjFromFirebase) => {
+          resolve(profileObjFromFirebase)
+        })
+        .error((error) => {
+          reject(error)
+        })
+      })
+    }
 
   let editParty = () => {
 
