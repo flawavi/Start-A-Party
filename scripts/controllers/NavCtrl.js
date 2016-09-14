e891b62dc1dd63f853f796e9433f62d18f36c8a1
Fletcher Watson <fbwthe6@gmail.com>
2016-09-14 11:59:00 -0500

    added basic navbar functionality. still needs work toggling login/logout buttons and assigning routes.

--- scripts/controllers/NavCtrl.js
+++ scripts/controllers/NavCtrl.js
@@ -1,2 +1,12 @@
 "use strict"
 
+app.controller("NavCtrl", function($scope, SearchTermData, $location){
+    $scope.navItems = [
+        {name: "Logout", url: "#/logout", showState: "$parent.isLoggedIn"},
+        {name: "Login", url: "#/login", showState: "!$parent.isLoggedOut"},
+        {name: "Start A Party", url: "#/party-form", showState: "$parent.isLoggedIn"},
+        {name: "Your Profile", url: "#/userprofile", showState: "$parent.isLoggedIn"}
+    ];
+    $scope.isActive = (viewLocation) => viewLocation === $location.path();
+
+});
