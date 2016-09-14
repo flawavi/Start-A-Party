dfcaffff936788c13dc2f8703a24805127146ae4
Fletcher Watson <fbwthe6@gmail.com>
2016-09-14 15:12:59 -0500

    formatting.

--- scripts/controllers/GeoLocateCtrl.js
+++ scripts/controllers/GeoLocateCtrl.js
@@ -3,36 +3,38 @@
 app.controller("GeoLocateCtrl", function($scope, $log, $timeout){
 
   $scope.message = "hello"
+
   $scope.map = { center: { latitude: 11.8251, longitude: 42.5903 }, zoom: 8}
+
   $scope.options = {scrollwheel: false};
+
   $scope.coordsUpdates = 0;
   $scope.dynamicMoveCtr = 0;
   $scope.marker = {
+    id: 0,
+    coords: {
+      latitude: 40.1451,
+      longitude: -99.6680
+    },
 
+    options: {draggable: true},
 
-  id: 0,
-  coords: {
-    latitude: 40.1451,
-    longitude: -99.6680
-  },
-  options: { draggable: true },
-  events: {
-    dragend: function (marker, eventName, args) {
-      $log.log('marker dragend');
-      var lat = marker.getPosition().lat();
-      var lon = marker.getPosition().lng();
-      $log.log(lat);
-      $log.log(lon);
-
-      $scope.marker.options = {
-        draggable: true,
-        labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-        labelAnchor: "100 0",
-        labelClass: "marker-labels"
-        };
+    events: {
+      dragend: function (marker, eventName, args) {
+        $log.log('marker dragend');
+        var lat = marker.getPosition().lat();
+        var lon = marker.getPosition().lng();
+        $log.log(lat);
+        $log.log(lon);
+        $scope.marker.options = {
+          draggable: true,
+          labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+          labelAnchor: "100 0",
+          labelClass: "marker-labels"
+        }
       }
     }
-  };
+  }
   $scope.$watchCollection("marker.coords", function (newVal, oldVal) {
     if (_.isEqual(newVal, oldVal))
       return;
